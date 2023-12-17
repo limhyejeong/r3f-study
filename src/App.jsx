2521557deba69be316b4e23fedc337b554b81627
limteractive <limteractive@gmail.com>
2023-12-17 15:18:56 +0900

    Add : '/' 경로 접속시 리다이렉션 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import ScrollAnimations from "./components/experiences/scroll_animations/ScrollAnimations";
 import MonsterStage from "./components/experiences/monster-stage/MonsterStage";
 
@@ -10,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/" element={<Navigate to="/r3f-study/" />} />
         <Route path="/r3f-study/" element={<Home />} />
         <Route
           path="/r3f-study/scroll-animations"
